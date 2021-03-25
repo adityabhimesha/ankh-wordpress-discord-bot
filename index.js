8ce7393806ca97a82ab204865ceeabc132c87e0d
Aditya Bhimesha <adityabhimesha@Adityas-MacBook-Pro.local>
2021-03-25 17:44:11 +0530

    initial changes

---
+++ index.js
@@ -0,0 +1,33 @@
+const express = require("express")
+const bodyParser = require("body-parser")
+
+const Discord = require('discord.js');
+const dotenv = require('dotenv');
+
+dotenv.config();
+const app = express()
+const PORT = 3000
+app.use(bodyParser.json())
+app.listen(PORT, () => console.log(`🚀 Server running on port ${PORT}`))
+
+
+app.post("/hook", (req, res) => {
+  console.log(req.body) 
+  res.status(200).end() 
+})
+
+//bot here
+console.log('Bot Started!-----');
+const client = new Discord.Client();
+
+client.on('ready', () => {
+    console.log(`Logged in as ${client.user.tag}!`);
+});
+
+client.on('message', msg => {
+    if (msg.content === 'Is Abhi the best trader in the world?') {
+        msg.reply('Hell Nah, he sucks!');
+    }
+});
+
+client.login(process.env.BOT_TOKEN);
