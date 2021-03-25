9ef6db5c2a657ac33bbbdd9c3d03b06b4bdfe72d
Aditya Bhimesha <adityabhimesha@Adityas-MacBook-Pro.local>
2021-03-25 17:51:25 +0530

    test change

--- index.js
+++ index.js
@@ -10,10 +10,10 @@ const PORT = 3000
 app.use(bodyParser.json())
 app.listen(PORT, () => console.log(`🚀 Server running on port ${PORT}`))
 
-
 app.post("/hook", (req, res) => {
-  console.log(req.body) 
-  res.status(200).end() 
+    console.log("Webhook Received!")
+    console.log(req.body) 
+    res.status(200).end() 
 })
 
 //bot here
