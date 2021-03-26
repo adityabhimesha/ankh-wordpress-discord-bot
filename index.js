f4ac1f0328d16d3e6ad19ddaebf4d79decf4584d
Aditya Bhimesha <adityabhimesha@Adityas-MacBook-Pro.local>
2021-03-26 18:55:05 +0530

    send channel message

--- index.js
+++ index.js
@@ -3,32 +3,38 @@ const bodyParser = require("body-parser")
 
 const Discord = require('discord.js');
 const dotenv = require('dotenv');
+const serverID = '761279321188335666'
+const channelName = '761279321871876101'
 
 dotenv.config();
 const app = express()
 const PORT = process.env.PORT || 3000
-app.use(bodyParser.json())
 app.listen(PORT, () => console.log(`🚀 Server running on port ${PORT}`))
 
-app.post("/hook", (req, res) => {
+app.post("/user_expires", (req, res) => {
     console.log("Webhook Received!")
-    console.log(req) 
+    var data = req.query
     
+    console.log(data.username)
+    console.log(data.user_email)
+
+    const channel = client.channels.cache.find(channel => channel.name === channelName)
+    channel.send(`Heads Up! ${data.username}, your monthly subscription has ended!`)
+
     res.status(200).end() 
 })
 
 //bot here
-console.log('Bot Started!-----');
+console.log('Bot Started!----->');
 const client = new Discord.Client();
 
 client.on('ready', () => {
     console.log(`Logged in as ${client.user.tag}!`);
-});
+    const list = client.guilds.cache.get(serverID); 
+    list.members.cache.filter(member => console.log(client.users.fetch(member.user.id)));
+    //offlineMembers = list.members.cache.filter(member => console.log(member));
 
-client.on('message', msg => {
-    if (msg.content === 'Is Abhi the best trader in the world?') {
-        msg.reply('Hell Nah, he sucks!');
-    }
+
 });
 
 client.login(process.env.BOT_TOKEN);
