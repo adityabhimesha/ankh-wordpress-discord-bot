bc86bb693dcd9fab5ad39aaaa4e43bb1dcf2fab5
Aditya Bhimesha <adityabhimesha@Adityas-MacBook-Pro.local>
2021-03-28 15:20:05 +0530

    guild member add

--- index.js
+++ index.js
@@ -1,10 +1,9 @@
 const express = require("express")
-const bodyParser = require("body-parser")
 
 const Discord = require('discord.js');
 const dotenv = require('dotenv');
 //change these to get onto ANKH FX server
-const serverID = '761279321188335666'
+//const serverID = ''
 const channelName = 'general';
 
 dotenv.config();
@@ -13,9 +12,9 @@ const PORT = process.env.PORT || 3000
 app.listen(PORT, () => console.log(`🚀 Server running on port ${PORT}`))
 
 app.get("/user_expires", (req, res) => {
-    console.log("Webhook Received!")
+    console.log("User Expired Webhook Received!")
     var data = req.query
-    console.log(data)
+    //console.log(data)
 
     const channel = client.channels.cache.find(channel => channel.name === channelName)
     
@@ -33,7 +32,7 @@ app.get("/user_expires", (req, res) => {
 })
 
 app.get("/user_renews", (req, res) => {
-    console.log("Renew Webhook Received!")
+    console.log("User Renew Webhook Received!")
     var data = req.query
     //console.log(data)
 
@@ -53,21 +52,10 @@ app.get("/user_renews", (req, res) => {
 })
 
 app.get("/new_user", (req, res) => {
-    console.log("Webhook Received!")
+    console.log("New User Webhook Received!")
     var data = req.query
-    //console.log(data)
-
-    const channel = client.channels.cache.find(channel => channel.name === channelName)
     
-    const msg = new Discord.MessageEmbed()
-	.setColor('#cfa710')
-	.setTitle(`Hello ${capitalize(data.username)},GREETINGS, WELCOME TO ANKH FX!`)
-	.setURL('https://www.ankhfx.com/')
-	.setDescription(`Hello ${capitalize(data.username)}, Welcome to the best community in the game, Please read through our #introduction channel to effectively monetize ANKH FX!, PLEASE do be patient while our admins provide access, THANK YOU!`)
-	.setThumbnail('https://ankhfxcom-8047ed.ingress-daribow.easywp.com/wp-content/uploads/2021/03/cropped-ankh-3-e1614877224480.png')
-	.setTimestamp()
-
-    channel.send(msg);
+    console.log(data)
 
     res.status(200).end() 
 })
@@ -86,6 +74,25 @@ client.on('ready', () => {
     console.log(`Logged in as ${client.user.tag}!`);
     //const list = client.guilds.cache.get(serverID); 
     //list.members.cache.filter(member => console.log(client.users.fetch(member.user.id)));
+
+
+});
+
+//WELCOME MEMBER
+client.on('guildMemberAdd', (member) => {
+    console.log(`Welcome Member called!`);
+
+    const channel = client.channels.cache.find(channel => channel.name === channelName)
+    
+    const msg = new Discord.MessageEmbed()
+	.setColor('#cfa710')
+	.setTitle(`Hello <@${capitalize(member.id)}>,GREETINGS, WELCOME TO ANKH FX!`)
+	.setURL('https://www.ankhfx.com/')
+	.setDescription(`Hello <@${capitalize(member.id)}>, Welcome to the best community in the game, Please read through our #introduction channel to effectively monetize ANKH FX!, PLEASE do be patient while our admins provide access, THANK YOU!`)
+	.setThumbnail('https://ankhfxcom-8047ed.ingress-daribow.easywp.com/wp-content/uploads/2021/03/cropped-ankh-3-e1614877224480.png')
+	.setTimestamp()
+
+    channel.send(msg);
 
 
 });
